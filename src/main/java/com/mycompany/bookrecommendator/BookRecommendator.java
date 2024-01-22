b2371b7468e6509f7c6344e8a62e8d00f3ce92e0
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2024-01-22 10:19:49 +0100

    initialize jena project

---
+++ src/main/java/com/mycompany/bookrecommendator/BookRecommendator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bookrecommendator;
+
+/**
+ *
+ * @author kassi
+ */
+public class BookRecommendator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
